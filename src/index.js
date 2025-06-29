277fbf46c126d7bb1a47e00c3cbc8a372c42ebdb
Kemal (via windows) <devkemal01@gmail.com>
2025-06-29 07:43:03 +0300

    add telegram sdk

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { init, backButton } from '@telegram-apps/sdk';
 import { createAppKit } from "@reown/appkit/react";
 import { networks, projectId, metadata, ethersAdapter } from "./configs/reown";
 import { BrowserRouter } from "react-router-dom";
@@ -24,6 +25,19 @@ createAppKit({
     "--w3m-accent": "#369",
   },
 });
+
+try {
+  init(); // Init the package and actualize all global dependencies.
+  backButton.mount(); // Mount the back button component and retrieve its actual state.
+
+  // When a user clicked the back button, go back in the navigation history.
+  const off = backButton.onClick(() => {
+    off();
+    window.history.back();
+  });
+} catch (error) {
+  console.error("Failed to initialize Telegram SDK:", error);
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
